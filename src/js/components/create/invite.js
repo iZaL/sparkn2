1aa99db8f60091c51f9b53b652f1a90505d7ef24
Anita <a.nita@hotmail.co.uk>
2016-12-15 12:47:10 +0000

    feat: sets up contact module natively plus adds ListView to invite component #39

--- src/js/components/create/invite.js
+++ src/js/components/create/invite.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import Contacts from 'react-native-contacts';
+import { View, ListView, Text } from 'react-native';
 import Router from '../../router';
 import Button from '../common/Button';
 import styles from '../../style';
 
 export default class Invite extends Component { // eslint-disable-line react/prefer-stateless-function
+
+  constructor (props) {
+    super(props);
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource: ds.cloneWithRows(['contact1', 'cinctact 2', 'contact1', 'cinctact 2',
+        'contact1', 'cinctact 2', 'contact1', 'cinctact 2',
+        'contact1', 'cinctact 2', 'contact1', 'cinctact 2', 'contact1', 'cinctact 2', 'contact1', 'cinctact 2'
+      ])
+    };
+  }
+  componentWillMount () {
+    Contacts.getAll((err, contacts) => {
+      if (err && err.type === 'permissionDenied') {
+        // x.x
+      } else {
+        console.log(contacts);
+      }
+    });
+  }
 
   nextPage () {
     this.props.navigator.push(Router.getRoute('confirm'));
@@ -12,7 +33,11 @@ export default class Invite extends Component { // eslint-disable-line react/pre
 
   render () {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={rowData => <Text>{rowData}</Text>}
+        />
         <Button
           buttonStyle={styles.buttonStyle}
           textStyle={styles.buttonTextStyle}
